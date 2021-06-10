e32a98e4d79ba1adcab7a42029896f51d4f488ae
Secondnd <2econdnd@gmail.com>
2021-06-10 19:48:56 +0200

    Spawn optimalization
    Enemies now dont get destroyed, but get disabled and unabled again

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -7,7 +7,7 @@ public class EnemyMover : MonoBehaviour
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
     [SerializeField] [Range(0f, 5f)] float TravelSpeed = 2f;
 
-    void Start()
+    void OnEnable()
     {
         FindPath();
         ReturnToStart();
@@ -49,6 +49,6 @@ public class EnemyMover : MonoBehaviour
             }
         }
 
-        Destroy(this.gameObject);
+        this.gameObject.SetActive(false);
     }
 }
